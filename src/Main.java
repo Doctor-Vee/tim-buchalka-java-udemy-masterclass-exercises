0f091ecafe457cb4383d08af675ecf703bd129cc
Victor-Chinewubeze <victor.chinewubeze@gmail.com>
2020-05-23 20:41:08 +0100

    feat: intialise the repo with existing codes

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import chapter4.MethodOverloading;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(MethodOverloading.calcFeetAndInchesToCentimeters(6, 10));
+    }
+}
